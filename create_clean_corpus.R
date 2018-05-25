7fde8a5f1faba365a73fdf08be2d9d4845aba4c9
jequihua <julian.equihua@gmail.com>
2018-05-25 17:58:12 -0500

    started to implement grid search

--- create_clean_corpus.R
+++ create_clean_corpus.R
@@ -3,7 +3,6 @@ library("tm")
 library("textreg")
 library("readxl")
 library("tidyverse")
-library("feather")
 
 # list sheets of an excel file
 sheets = excel_sheets("./data/2011-2016 todo 11_10_UKPF cities.xlsx")
@@ -13,13 +12,24 @@ sheets # some repeated sheets for no reason
 # load first sheet
 sheet = read_excel("./data/2011-2016 todo 11_10_UKPF cities.xlsx",sheet=sheets[1])
 
+# no olvidar cambiar nombres de campos
+# Entidad -> Estado
+# FOLIO -> Folio
+# Presupuesto -> Total
+# Nombre de Proyecto -> Destino
+
 # select only our variables of interest (textid, description, class)
-full_table = sheet %>% select(Folio,Destino,Tipo)
+full_table = sheet %>% select(Folio,Destino,Estado,Total,Tipo)
 
 # vertically concatenate next sheets (ignoring repeated ones)
-for (i in seq(2,10,2))
+for (i in seq(3,11,2))
 {
-  sheet = read_excel("./data/2011-2016 todo 11_10_UKPF cities.xlsx",sheet=sheets[i]) %>% select(Folio,Destino,Tipo)
+  sheet = read_excel("./data/2011-2016 todo 11_10_UKPF cities.xlsx",sheet=sheets[i]) %>% select(Folio,Destino,Estado,Total,Tipo)
+  
+  print(nrow(sheet))
+  
+  print(head(sheet))
+  
   full_table = rbind(full_table,sheet)
 }
 
@@ -28,11 +38,15 @@ full_table = full_table %>% filter(complete.cases(.))
 
 ### add (unlabeled) 2016 data
 
+# dont forget to manually delete the first 9 rows of the 2017 excel file
+
 # read only sheet for 2016
-sheet = read_excel("./data/presupuesto 2017.xlsx",sheet=1,skip=6) 
+sheet = read_excel("./data/presupuesto 2017.xlsx",sheet=1) 
+
+head(sheet)
 
 # data table has a bad row as names delete first row and select only columns we want
-sheet = sheet[-1,] %>% select(1:2)
+sheet = sheet %>% select(Folio,Destino,Estado,Total)
 
 # clean cases (no missing values in any field)
 sheet = sheet %>% filter(complete.cases(.))
@@ -40,13 +54,8 @@ sheet = sheet %>% filter(complete.cases(.))
 # add a variable for "Tipo" even though it's not present (remember these cases are unlabeled)
 sheet$Tipo=NA
 
-# change names to fit with full table
-names(sheet)=c("Folio","Destino","Tipo")
-
 # concatenate with full table
 full_table = rbind(full_table,sheet)
-
-tail(full_table)
 
 # make corpus from sms_messages and clean (to lowercase, remove numbers, punctuation and extra spaces)
 corpus = Corpus(VectorSource(full_table$Destino))
@@ -59,4 +68,6 @@ corpus = tm_map(corpus, stripWhitespace)
 full_table$Destino = iconv(get("content", corpus), to='ASCII//TRANSLIT')
 
 # save as RData
-#save(full_table, file="./data/full_table.RData")
+save(full_table, file="./data/full_corpus.RData")
+
+head(full_table)
